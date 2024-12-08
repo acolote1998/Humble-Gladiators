2f4bc189e292513edd2f2742ebdda69b266e000f
acolote1998 <manuelacevedo.ar@gmail.com>
2024-12-08 20:51:54 +0100

    Home Scene Displaying Gladiator

--- js/home.js
+++ js/home.js
@@ -5,7 +5,7 @@ function getHeroIDfromURL(param) {
 var idHero = getHeroIDfromURL("id");
 var gladiators = [];
 
-function sendData() {
+function getHeroFromDB() {
   // Send this value to the PHP script using fetch
   fetch("sql/homeLoadHero.php", {
     method: "POST", // Or GET, depending on your need
@@ -45,4 +45,42 @@ function sendData() {
       }
     })
     .catch((error) => console.error("Error:", error));
+}
+
+function loadHeroToDocument() {
+  document.getElementById("heroName").innerText = gladiators[0].name;
+
+  document.getElementById("heroHPBarCreationScreen").value =
+    gladiators[0].hp + " / " + gladiators[0].maxHP;
+
+  let heroFace = document.getElementById("heroFace");
+
+  // Set the background size
+  heroFace.style.backgroundSize = "100% 100%";
+
+  // Set the width and height
+  heroFace.style.width = gladiators[0].weight + "px"; // Heads width is 50% of bodies width
+  heroFace.style.height = ((15 * gladiators[0].height) / 90) * 3 + "px"; // Adjust head height based on body height
+
+  // Set the background image
+  heroFace.style.backgroundImage = "url(" + gladiators[0].headSRC + ")";
+
+  let heroBody = document.getElementById("heroBody");
+  heroBody.style.backgroundSize = "100% 100%";
+  heroBody.style.height = gladiators[0].height * 2 + "px"; // Body height
+  heroBody.style.width = gladiators[0].weight * 2 + "px"; // Body width
+  heroBody.style.backgroundImage = "url(" + gladiators[0].bodySRC + ")";
+
+  document.getElementById("heroWeaponCreationScrenText").style.backgroundImage =
+    "url(" + gladiators[0].weaponSRC + ")";
+  document.getElementById("heroStrenghtCreationScrenText").innerText =
+    gladiators[0].strength;
+  document.getElementById("heroDexterityCreationScrenText").innerText =
+    gladiators[0].dexterity;
+  document.getElementById("heroConstitutionCreationScrenText").innerText =
+    gladiators[0].constitution;
+  document.getElementById("heroLuckCreationScrenText").innerText =
+    gladiators[0].luck;
+  document.getElementById("heroSpeedCreationScrenText").innerText =
+    gladiators[0].speed;
 }
